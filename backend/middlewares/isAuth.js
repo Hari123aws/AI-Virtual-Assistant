49c2de14ba362bec105c206a846dce123e9da21c
Harish <harishhatrics7@gmail.com>
2025-10-10 20:00:40 +0530

    Update isAuth.js

--- backend/middlewares/isAuth.js
+++ backend/middlewares/isAuth.js
@@ -1,15 +1,15 @@
 import jwt from "jsonwebtoken"
-const isAuth= async(req,res,next)=>{
+const isAuth=async (req,res,next)=>{
     try {
-        const token=req.cookies?.token || req.header("Authorization")?.replace("Bearer ", "");
+        const token=req.cookies.token
         if(!token){
             return res.status(400).json({message:"token not found"})
         }
-        const verifyToken=await jwt.verify(token,process.env.
-            JWT_SECRET)
+        const verifyToken=await jwt.verify(token,process.env.JWT_SECRET)
         req.userId=verifyToken.userId
 
-        next();
+        next()
+
     } catch (error) {
         console.log(error)
         return res.status(500).json({message:"is Auth error"})
